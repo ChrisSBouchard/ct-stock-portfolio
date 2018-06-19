036ca1177a26e4b189fd153f2da56d414b2628c5
Chris Bouchard <cbouchar@umd.edu>
2018-06-19 19:01:21 -0400

    Lab 2

--- StockPortfolio/Program.cs
+++ StockPortfolio/Program.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace StockPortfolio
 {
-    class Program
-    {
+    class Program { 
+
         static void Main(string[] args)
         {
             Console.Out.WriteLine("Welcome to stock portfolio.");
-            Dictionary<string, int> portfolio = new Dictionary<string, int>();
+            StreamReader r = new StreamReader("portfolio.json");
+            Dictionary<string, int> portfolio = JsonConvert.DeserializeObject<Dictionary<string, int>>(r.ReadToEnd());
+            r.Close();
             Boolean menu = true;
             while (menu)
             {
-                Console.Out.WriteLine("Enter buy, sell, view, export, or quit: ");
+                Console.Out.WriteLine("Enter price, buy, sell, view, export, or quit: ");
                 string menuinput = Console.ReadLine();
                 switch (menuinput)
                 {
+                    case "price":
+                        GetStockPrice();
+                        break;
                     case "buy":
                         portfolio = Buy(portfolio);
                         break;
@@ -42,10 +49,22 @@ namespace StockPortfolio
 
         }
 
+        private static void GetStockPrice()
+        {
+            Console.Out.WriteLine("Enter stock symbol: ");
+            string stock = Console.ReadLine().ToUpper();
+            string uri = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stock + "&interval=1min&apikey=2460Q7OX2FHSSX1T";
+            string str = new WebClient().DownloadString(uri);
+            JObject json = JObject.Parse(str);
+            string time = (string)json["Meta Data"]["3. Last Refreshed"];
+            string price = (string)json["Time Series (1min)"][time]["4. close"];
+            Console.WriteLine("{0}", price);
+        }
+
         private static void Export(Dictionary<string, int> portfolio)
         {
             string json = JsonConvert.SerializeObject(portfolio);
-            File.WriteAllText(@"C:\Users\Chris\source\repos\StockPortfolio\StockPortfolio\portfolio.json", json);
+            File.WriteAllText(@"portfolio.json", json);
         }
 
         private static void View(Dictionary<string, int> portfolio)
